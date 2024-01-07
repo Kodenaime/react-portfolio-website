4fc646d873902015eb408da3a83e99c7f57bbd5e
iamkodenaime <creativedannyk@gmail.com>
2024-01-07 23:20:53 +0100

    finished with contact section

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,9 +1,70 @@
 import React from 'react'
 import './contact.css'
+import { MdMarkEmailRead } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaSquareWhatsapp } from "react-icons/fa6";
+import  { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+
+  const form = useRef();
+
+    const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_6436wtt', 'template_03g5tg6', form.current, 'ixOpuARCs9lBJOb0F')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset()
+
+  };
+
+
   return (
-    <div>Contact</div>
+    <section id="contact">
+      <h5>get in touch</h5>
+      <h2>Contact</h2>
+
+      <div className="container contact-container">
+
+        <div className="contact-left">
+          <article className="contact">
+            <MdMarkEmailRead className='contact-icon'/>
+            <h4>Email</h4>
+            <h5>creativedannyk@gmail.com</h5>
+            <a href="mailto:creativedannyk@gmail.com" target='_blank'>Send a messge</a>
+          </article>
+          <article className="contact">
+            <FaLinkedin className='contact-icon'/>
+            <h4>Linkedin</h4>
+            <h5>Ekechukwu Daniel</h5>
+            <a href="" target='_blank'>Send a messge</a>
+          </article>
+          <article className="contact">
+            <FaSquareWhatsapp className='contact-icon'/>
+            <h4>Whatsapp</h4>
+            <h5>Ekechukwu Daniel</h5>
+            <a href="https://api.whatsapp.com/send?phone=+2348068564195" target='_blank'>Send a messge</a>
+          </article>
+        </div>
+        
+        <div className="container-right">
+          <form ref={form} onSubmit={sendEmail}>
+            <input type="text" name='name' placeholder='Your Full name' required />
+            <input type="email" name='email' placeholder='Your email' required />
+            <textarea name="message" rows="10" placeholder='Your message' required></textarea>
+            <button type='submit' className='btn btn-primary'>Send</button>
+          </form>
+        </div>
+
+      </div>
+      
+    </section>
   )
 }
 
--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,9 +1,90 @@
 import React from 'react'
 import './portfolio.css'
+import port1 from '../../assets/port1.png'
+import port2 from '../../assets/port2.png'
+import port3 from '../../assets/port3.png'
+import port4 from '../../assets/port4.png'
+import port5 from '../../assets/port5.png'
+import port6 from '../../assets/port6.png'
+
+
+const data = [
+
+  {
+    id: 1,
+    image: port1,
+    title: 'A Portfolio Website',
+    github: '/Kodenaime/My-Portfolio',
+    demo: 'https://resonant-boba-a968b2.netlify.app/'
+  },
+  {
+    id: 2,
+    image: port2,
+    title: 'Annas Closet A Display website',
+    github: 'https://github.com/Kodenaime/Anna-s-Closet',
+    demo: 'https://kodenaime.github.io/Anna-s-Closet/'
+  },
+  {
+    id: 3,
+    image: port3,
+    title: 'Absu Blog- A functional blog',
+    github: 'https://github.com/Kodenaime/Absu-Blog',
+    demo: 'https://kodenaime.github.io/Absu-Blog/'
+  },
+  {
+    id: 4,
+    image: port4,
+    title: 'A react portfolio',
+    github: 'https://github.com/Kodenaime/React-portfolio.git',
+    demo: 'https://kodenaime.github.io/React-portfolio/'
+  },
+  {
+    id: 5,
+    image: port5,
+    title: 'A school management system',
+    github: 'https://github.com/Kodenaime/Pub-Class.git',
+    demo: 'https://kodenaime.github.io/Pub-Class/'
+  },
+  {
+    id: 6,
+    image: port6,
+    title: 'A website for a digital agency',
+    github: 'https://github.com/Kodenaime/digital-agency-.git',
+    demo: 'https://kodenaime.github.io/digital-agency-/'
+  },
+
+]
+
+
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id="portfolio">
+      <h5>Some of my Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio-container">
+
+       
+        {
+          data.map(({id, image, title, github, demo}) => {
+              return (
+                <article key={id} className="portfolio">
+                    <div className="portfolio-image">
+                    <img src={image} alt={title} />
+                    </div>
+                    <h3>{ title }</h3>
+                    <div className="portfolio-cta">
+                    <a href={ github } className='btn' target='_blank'>Github</a>
+                    <a href={ demo } className='btn btn-primary' target='_blank'>Live Demo</a>
+                    </div>               
+                </article>
+              )
+          })
+        }
+      </div>
+
+    </section>
   )
 }
 
